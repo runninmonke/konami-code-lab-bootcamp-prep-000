d261d0807232e36ed6926224afcfccc1a4f4e73f
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-09-13 23:45:48 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   let index = 0;
 
-  document.querySelect('body').addEventListener('keydown', function(e){
+  document.querySelector('body').addEventListener('keydown', function(e){
     let key = parseInt(e.detail || e.which);
     if (key === code[i]) {
       index++;
