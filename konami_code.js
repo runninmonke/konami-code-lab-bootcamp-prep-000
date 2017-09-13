a7c53206295bb68213c31826af4a29b027b0aa1e
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-09-13 23:40:45 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,19 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  let index = 0
+
+  document.querySelect('body').addEventListener('keydown', function(e){
+    const key = parseInt(e.detail || e.which)
+    if (key === code[i]) {
+      index++;
+      if (index === code.length) {
+        alert('Konami!')
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  })
 
 }
